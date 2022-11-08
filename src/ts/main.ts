807b2f1e8a3247862c6ba1449dc15123a8fe7556
Ziplodocus <zirpho@gmail.com>
2022-11-08 19:24:33 +0000

    Remove option prefills

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,44 +1,38 @@
-import { get, getAll } from './utilities';
+import { get } from './utilities';
 import EncounterForm from './EncounterForm';
 import Notice from './Notice';
 import * as Storage from './Storage';
 
 customElements.define('notice-strip', Notice);
-const noticeBoard = get('notice-strip') as Notice;
+const notice = get('notice-strip') as Notice;
 const keyInput = get('[data-auth-token]') as HTMLInputElement;
 
-getAll('[data-encounter-form]').forEach(el => {
-    if (!(el instanceof HTMLFormElement)) return console.error(el, 'Not a form element');
+const form = get('form[data-encounter-form]') as HTMLFormElement;
+new EncounterForm(get('[data-submit]') as HTMLButtonElement, form);
 
-    const { form, options } = new EncounterForm(get('[data-submit]') as HTMLButtonElement, el);
+//@ts-ignore TS doesn't recognise custom events.
+form.addEventListener('err', (e: CustomEvent<Error>) => {
+    notice.error(e.detail.message);
+});
+//@ts-ignore TS doesn't recognise custom events.
+form.addEventListener('notice', (e: CustomEvent<string>) => {
+    notice.notify(e.detail);
+});
+
+form.addEventListener('zubmit', async (e) => {
     //@ts-ignore TS doesn't recognise custom events.
-    form.addEventListener('err', (e: CustomEvent<Error>) => {
-        noticeBoard.error(e.detail.message);
-    });
-    //@ts-ignore TS doesn't recognise custom events.
-    form.addEventListener('notice', (e: CustomEvent<string>) => {
-        noticeBoard.notify(e.detail);
-    });
+    const { filename, data: data } = e.detail;
+    const key = keyInput.value;
 
-    options.add();
-    options.namer.value = 'bumble';
-    options.add();
+    // Check if file exists
+    const fileExists = await Storage.exists(filename, key);
+    if (fileExists === true) return notice.error(`Encounter ${data.title} already exists!`);
+    else if (fileExists instanceof Error) return notice.error(fileExists.message);
 
-    form.addEventListener('zubmit', async (e) => {
-        //@ts-ignore TS doesn't recognise custom events.
-        const { filename, data: data } = e.detail;
-        const key = keyInput.value;
+    const uploadResult = await Storage.upload(filename, data, key);
+    if (uploadResult instanceof Error) {
+        notice.error(uploadResult.message);
+    }
 
-        // Check if file exists
-        const fileExists = await Storage.exists(filename, key);
-        if (fileExists === true) return noticeBoard.error(`Encounter ${data.title} already exists!`);
-        else if (fileExists instanceof Error) return noticeBoard.error(fileExists.message);
-
-        const uploadResult = await Storage.upload(filename, data, key);
-        if (uploadResult instanceof Error) {
-            noticeBoard.error(uploadResult.message);
-        }
-
-        noticeBoard.notify(`Encounter ${data.title} has been added`);
-    });
+    notice.notify(`Encounter ${data.title} has been added`);
 });
